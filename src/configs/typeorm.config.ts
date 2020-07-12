5f58fb748373ce6d5c49a5051f42dd87f239028d
Jefferson Silva <jgsnat@gmail.com>
2020-07-11 23:45:42 -0300

    - adoption of environment variables for database connection configurations;

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,13 @@
+import 'dotenv/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'pguser',
-    password: 'pgpassword',
-    database: 'nestjs',
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true
+    synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE) 
 };
